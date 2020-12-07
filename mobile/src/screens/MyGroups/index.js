7302df8b5a8407b5bdb5f5c882b0eeb44fc83417
Fernando Severino Almeida <fernandosevjipa@gmail.com>
2020-12-07 07:31:41 -0400

    update mysql script

--- mobile/src/screens/MyGroups/index.js
+++ mobile/src/screens/MyGroups/index.js
@@ -24,6 +24,12 @@ export default function MyGroups({navigation}) {
   const [modalVisible, setModalVisible] = React.useState(false);
   const [groupName, setGroupName] = React.useState('');
 
+  const [messageModalVisible, setMessageModalVisible] = React.useState(false);
+  const [message, setMessage] = React.useState('');
+  const sendMessageLoading = useSelector(
+    (state) => state.group.sendMessageLoading,
+  );
+
   React.useEffect(() => {
     dispatch(getMyGroupsRequest(toast));
   }, []);
@@ -43,7 +49,13 @@ export default function MyGroups({navigation}) {
       <Container>
         <Scroll>
           {myGroups.map((value, index) => {
-            return <GroupRow key={index} name={value.group_name} />;
+            return (
+              <GroupRow
+                key={index}
+                name={value.group_name}
+                onPress={() => setMessageModalVisible(!messageModalVisible)}
+              />
+            );
           })}
         </Scroll>
         <NewGroupContainer onPress={() => setModalVisible(true)}>
@@ -63,6 +75,23 @@ export default function MyGroups({navigation}) {
               title="Nome do grupo"
               value={groupName}
               onChangeText={(text) => setGroupName(text)}
+            />
+          </ContainerModal>
+        </Dialog>
+
+        <Dialog
+          btnAction={() => console.log('message')}
+          btnTitle="Enviar"
+          close={() => setMessageModalVisible(false)}
+          title="Nova mensagem"
+          visible={messageModalVisible}
+          loading={false}>
+          <ContainerModal>
+            <Input
+              color={colors.white}
+              title="Digite sua mensagem"
+              value={message}
+              onChangeText={(text) => setMessage(text)}
             />
           </ContainerModal>
         </Dialog>
--- msginst.sql
+++ msginst.sql
@@ -20,32 +20,3 @@ CREATE TABLE IF NOT EXISTS `user_group` (
     CONSTRAINT fk_user_id FOREIGN KEY (user_id)  REFERENCES `user`(`user_id`),
     CONSTRAINT fk_group_id FOREIGN KEY (group_id)  REFERENCES `group`(`group_id`)
 );
-
-select * from user_group;
-select * from `group`;
-select * from user;
-insert into user_group (user_group_id, user_id, group_id) values (default, 2, 2);
-insert into user_group (user_group_id, user_id, group_id) values (default, 2, 7);
-
-insert into user values (default, 'A', 'A2', 'AA');
-
-select 
-	`group`.`group_id`, `group`.`group_name`, `user`.`user_id`
-from `group`, `user_group`, `user`
-where `group`.`group_id` = `user_group`.`group_id` and `user`.`user_id` = `user_group`.`user_id` and `user`.`user_id` = 1;
-
-select * from `group` where `group`.`group_id` = (select 
-	`user_group`.`group_id`
-from `user_group`
-where `user_group`.`user_id` != 1);
-
-select `group`.`group_name` 
-from `group` 
-inner join `user_group` 
-on `group`.`group_id` = `user_group`.`group_id` and `group`.`group_id` = `user_group`.`group_id` 
-where `user_group`.`user_id` != 1;
-
-
-select * from `user_group`;
-select * from `group`;
-select * from user;
