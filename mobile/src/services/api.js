b5edc6499f87962e319c29ed303b0e55935a0abe
Fernando Severino Almeida <fernandosevjipa@gmail.com>
2020-12-07 11:17:22 -0400

    OneSignal Cofiguration

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -5,7 +5,7 @@ import {API_BASE_URL} from '@env';
 export const {CancelToken, isCancel} = axios;
 
 const api = axios.create({
-  baseURL: Platform.OS === 'ios' ? API_BASE_URL : 'http://10.0.2.2:3333',
+  baseURL: API_BASE_URL,
 });
 
 export default api;
